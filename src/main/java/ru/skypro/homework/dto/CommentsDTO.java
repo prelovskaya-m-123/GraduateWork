d3cdb27cacb0598708bc3bac0b4ce9129a3de4df
mariaaraslanova <maraslanowa@yandex.ru>
2026-04-08 21:56:45 +0300

    Week 4 assignment done

--- src/main/java/ru/skypro/homework/dto/CommentsDTO.java
+++ src/main/java/ru/skypro/homework/dto/CommentsDTO.java
@@ -2,8 +2,11 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 import java.util.List;
+import lombok.Builder;
+
 
 @Data
+@Builder
 public class CommentsDTO {
     private Integer count;
     private List<CommentDTO> results;
--- src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import ru.skypro.homework.dto.UserDTO;
 import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.model.User;
 import ru.skypro.homework.repository.UserRepository;
+import ru.skypro.homework.service.ImageService;
 import ru.skypro.homework.service.UserService;
 
 import java.io.IOException;
@@ -23,6 +24,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserMapper userMapper;
+    private final ImageService imageService;
 
     @Override
     public UserDTO getCurrentUser() {
@@ -66,7 +68,7 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
 
         try {
-            String imageUrl = saveImage(image);
+            String imageUrl = imageService.saveImage(image, "users");
             user.setImage(imageUrl);
             userRepository.save(user);
         } catch (IOException e) {
